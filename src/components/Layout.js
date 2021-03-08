41f8b26ea06cad75dc5c14258855d1cb03b97f03
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-08 16:00:02 +0530

    adding themeprovider

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,8 @@ import TopBar from './Topbar.js'
 
 
 import Button from '@material-ui/core/Button'
+import {ThemeProvider} from '@material-ui/core/styles'
+import theme from '../Theme'
 
 
 
@@ -11,8 +13,10 @@ export default class Layout extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <ThemeProvider theme={theme}>
         <TopBar/>
         {this.props.children}
+      </ThemeProvider>
       </React.Fragment>
       )
   }
