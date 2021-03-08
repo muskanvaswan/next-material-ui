cbbc3dbf7b2f04824c4aea2421a694c7bc8e9c25
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-08 15:18:28 +0530

    making index page structure

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Container from '@material-ui/core/Container'
 import Layout from '../src/components/Layout.js'
-
+import Banner from '../src/components/Index/Banner'
 
 export default function Home() {
   return (
     <Container>
-      <Layout />
+      <Layout>
+        <Banner />
+      </Layout>
     </Container>
 
   )
