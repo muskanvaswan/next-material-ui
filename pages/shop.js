c1611eb84fd3ed9314a9c5b51ad60b5463182df2
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-09 22:04:25 +0530

    adding fade in animation to shop page

--- pages/shop.js
+++ pages/shop.js
@@ -8,19 +8,27 @@ import Items from '../src/components/Shop/Items'
 import Hero from '../src/components/Shop/Hero'
 
 
+import Fade from '@material-ui/core/Fade';
 
 
 export default function Album() {
 
+  const trans = {
+    enter: 3000,
+    exit: 2000
+  }
   return (
     <Layout bright>
       <React.Fragment>
         <CssBaseline />
-
+        <Fade in timeout={trans}>
         <main>
+
           <Hero />
+
           <Items />
         </main>
+        </Fade>
 
       </React.Fragment>
     </Layout>
