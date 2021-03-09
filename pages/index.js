98efed142101c2e19c236c4d85e6ff55ba0a8e37
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-09 21:04:43 +0530

    removing footer from index page

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Banner from '../src/components/Index/Banner'
 export default function Home() {
   return (
     <Container>
-      <Layout short>
+      <Layout noFooter>
         <Banner />
       </Layout>
     </Container>
