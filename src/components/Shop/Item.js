296eefe9f4223e447bfaa806c135d1269f01ad24
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-09 22:53:39 +0530

    made seperate component fro single item

---
+++ src/components/Shop/Item.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton'
+
+import ShoppingCart from '@material-ui/icons/ShoppingCart'
+import FavoriteBorderTwoToneIcon from '@material-ui/icons/FavoriteBorderTwoTone';
+const useStyles = makeStyles((theme) => ({
+
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Item(props){
+  const classes = useStyles();
+
+  return (
+    <Grid item key={props.card} xs={12} sm={6} md={4}>
+      <Card className={classes.card}>
+
+
+          <CardMedia
+            className={classes.cardMedia}
+            image={props.img}
+            title="Image title"
+          />
+
+        <CardContent className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.heading}
+          </Typography>
+          <Typography>
+            {props.children}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <IconButton size="small" color="primary">
+            <ShoppingCart/>
+          </IconButton>
+          <IconButton size="small" color="primary">
+            <FavoriteBorderTwoToneIcon/>
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Grid>
+  )
+}
