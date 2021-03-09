9c4d8ce0f90ccc410aaf1d88e3e386c384e78b02
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-09 19:52:08 +0530

    making blue theme in theme.js

--- src/Theme.js
+++ src/Theme.js
@@ -2,28 +2,37 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import grey from '@material-ui/core/colors/grey';
 import red from '@material-ui/core/colors/red';
 
-let colorOne = red[900]
-let colorTwo = grey[900]
-let bg = 'rgba(255, 255, 255, 1)'
+let colorOne = '#66FCF1'
+let colorTwo = '#fff'
+let colorThree = '#1F2833'
+let colorFour = '#45A29E'
+let bg = '#0B0C10'
+let paper = 'rgba(#1c2136, 1)'
 
 const theme = createMuiTheme({
   palette: {
     background: {
-      paper: colorOne,
+      paper: colorThree,
       default: bg,
     },
     type: 'dark',
     text:{
       primary: colorTwo,
+
+    },
+    typography: {
+      h1: colorOne,
+      subtitle1: colorThree,
     },
 
     primary: {
       main: colorOne,
-      contrastText: 'white',
+      contrastText: bg,
     },
     secondary: {
-      main: colorTwo,
+      main: colorFour,
     },
+
   },
 });
 
