5e9e645bda4a252d47f77360847b58ac035d2547
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-11 02:24:01 +0530

    adding link to go back to shop page

--- src/components/Products/More.js
+++ src/components/Products/More.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -26,6 +28,10 @@ export default function More(){
 
         Where does it come from?
         Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+      </Typography>
+      <Typography align="right">
+        <Button href="/shop" color="primary" startIcon={<ArrowBackIosIcon />}>  Go back to Catalogue</Button>
 
       </Typography>
     </Container>
