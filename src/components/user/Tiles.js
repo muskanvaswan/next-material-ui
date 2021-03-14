6676abcc0fcf50ed73dc88e190234469510b1d24
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-14 23:20:10 +0530

    fixing error in tiles page

--- src/components/user/Tiles.js
+++ src/components/user/Tiles.js
@@ -3,10 +3,12 @@ import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
 
 export default function Tiles(props){
+  const tiles = [1, 2, 3, 4]
+
   return (
-    const tiles = [1, 2, 3, 4]
+
     <React.Fragment>
-      <GridList cellHeight={160} className="" cols={3}>
+      <GridList cellHeight={160} className="" cols={4}>
         {tiles.map((tile) => (
           <GridListTile key={tile} cols={2}>
             <img src="https://source.unsplash.com/random" alt={tile} />
