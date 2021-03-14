026f3dddee5c28044d4bfbae7e4b4fdeb8dec357
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-14 22:36:25 +0530

    Initializig order summary tiles

---
+++ src/components/user/Tiles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid'
+
+export default function Tiles(props){
+  return (
+    <React.Fragment>
+      <Grid container>
+        <Grid item></Grid>
+
+      </Grid>
+    </React.Fragment>
+  )
+}
