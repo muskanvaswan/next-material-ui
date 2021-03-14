ac25a144a615f198321ab7743151c6906ab8d845
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-15 01:25:28 +0530

    trying to reduce height of tiles

--- src/components/user/Tiles.js
+++ src/components/user/Tiles.js
@@ -2,15 +2,28 @@ import React from 'react';
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  tileImage: {
+    maxHeight: '50%',
+  },
+  tileContainer: {
+    maxHeight: '100%'
+  }
+
+}))
+
 export default function Tiles(props){
   const tiles = [1, 2, 3, 4]
+   const classes = useStyles();
 
   return (
 
     <React.Fragment>
-      <GridList cellHeight={160} className="" cols={4}>
+      <GridList cellHeight={160} className="" cols={4} className={classes.tileContainer}>
         {tiles.map((tile) => (
-          <GridListTile key={tile} cols={2}>
+          <GridListTile key={tile} cols={2} className={classes.tileContainer}>
             <img src="https://source.unsplash.com/random" alt={tile} />
           </GridListTile>
         ))}
