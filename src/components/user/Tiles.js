467c19b3dcff3dfcb05d3b3845440c0260e3cd58
Muskan Vaswan <muskanvaswan@gmail.com>
2021-03-18 23:03:02 +0530

    fixing height for tiles to be exact fit

--- src/components/user/Tiles.js
+++ src/components/user/Tiles.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import GridList from '@material-ui/core/GridList'
 import GridListTile from '@material-ui/core/GridListTile'
+import Grid from '@material-ui/core/Grid'
 
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   tileImage: {
-    maxHeight: '50%',
+    height: '100%',
+    width: '100%',
   },
   tileContainer: {
-    maxHeight: '100%'
+    height: 'max-content'
   }
 
 }))
@@ -21,11 +23,11 @@ export default function Tiles(props){
   return (
 
     <React.Fragment>
-      <GridList cellHeight={160} className="" cols={4} className={classes.tileContainer}>
+      <GridList cellHeight={100} className="" cols={2}>
         {tiles.map((tile) => (
-          <GridListTile key={tile} cols={2} className={classes.tileContainer}>
-            <img src="https://source.unsplash.com/random" alt={tile} />
-          </GridListTile>
+          <Grid item key={tile} className={classes.tileContainer}>
+            <img src="https://source.unsplash.com/random" alt={tile} className={classes.tileImage}/>
+          </Grid>
         ))}
       </GridList>
     </React.Fragment>
